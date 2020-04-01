81f76034c0593900d4659bc034d27b648e9051b8
Siddharth Srivastava <akssps011@gmail.com>
2020-04-02 00:30:47 +0530

    newsapi changes

--- media/chatbot/js/chatbot.js
+++ media/chatbot/js/chatbot.js
@@ -509,7 +509,8 @@ function newsfeed() {
 // alert("Inside newsfeed");
 //var myurl = 'https://newsapi.org/v2/everything?qInTitle=+corona%20+india&pageSize=6&apiKey=db7f4ed8533240f39bafa44572908166';
 //var myurl = 'https://newsapi.org/v2/top-headlines?country=in&category=health&q=+corona%20+india&pageSize=20&apiKey=db7f4ed8533240f39bafa44572908166'
-var myurl = 'https://newsapi.org/v2/top-headlines?q=corona%20india&pageSize=20&apiKey=db7f4ed8533240f39bafa44572908166'
+//var myurl = 'https://newsapi.org/v2/top-headlines?category=health&q=corona%20india&pageSize=20&apiKey=db7f4ed8533240f39bafa44572908166'
+var myurl = 'https://newsapi.org/v2/top-headlines?country=in&q=corona&pageSize=20&apiKey=db7f4ed8533240f39bafa44572908166'
 $.ajax({
 	type: "GET",
 	url: myurl,
