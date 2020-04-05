233d00add0fc3ed6d491e7d9420d04ba01316b63
Prashant Mishra <prashantmindia@gmail.com>
2020-04-06 00:12:16 +0530

    heat map implementation

--- media/misc/js/mapNcharts.js
+++ media/misc/js/mapNcharts.js
@@ -36,7 +36,7 @@ function initMap() {
   };
 
   // Display a map on the web page
-  map = new google.maps.Map(document.getElementById("map"), mapOptions);
+  map = new google.maps.Map(document.getElementById("googleMapNcovCas"), mapOptions);
   map.setTilt(50);
 
   // Add multiple markers to map
@@ -150,7 +150,10 @@ var ajaxLatestCases = $.ajax({
       generateDonutChart(result.data.summary);
 
     //generate and set markers coordinate and marker html for map
-      generateMapMarkers(result.data.regional);
+      //generateMapMarkers(result.data.regional);
+
+    //generate am chart series for covid19 satewise cases
+      generateAmChartCovCasSeries(result.data.regional);
   },
   error: function(results) {
     alert("There is an error. " + results.stringfy);
@@ -197,6 +200,29 @@ function setDashboardStats(statsSummary) {
   $('#deaths').html(JSON.stringify(statsSummary.deaths));
   $('#clu').html(JSON.stringify(statsSummary.confirmedButLocationUnidentified));
 
+}
+
+//generate am chart series for covid19 satewise cases
+function generateAmChartCovCasSeries (regionalData) {
+  gentAmChartCovCasSeriesDiffObj = jQuery.Deferred();
+  amChartSateCovCasSeries.length=0;
+  for (key in regionalData) {
+    var apiInSateDtl = regionalData[key]
+    var apiInSateName = apiInSateDtl.loc;
+    var inConfCases = apiInSateDtl.confirmedCasesIndian;
+    var frnConfCases = apiInSateDtl.confirmedCasesForeign;
+    var dischargedCont = apiInSateDtl.discharged;
+    var deathCont = apiInSateDtl.deaths;
+
+    var inStateDtlHardCode = cordinatList.indianState;
+    if (indianCharSateCode[apiInSateName]!=null ||indianCharSateCode[apiInSateName]!=undefined ) {
+        var stCharCode = indianCharSateCode[apiInSateName];
+        amChartSateCovCasSeries.push({id: stCharCode, value: inConfCases+frnConfCases});
+      } else {
+        console.log("Add this sate char code for: "+apiInSateName);
+      }
+  }
+  gentAmChartCovCasSeriesDiffObj.resolve();
 }
 
 //generate and set markers coordinate and marker html for map
@@ -581,7 +607,7 @@ function drawChartStateWise( data){
     am4core.useTheme(am4themes_material);
   // Themes end
   var chart = am4core.create("chartdiv", am4charts.XYChart);
-  
+
 
 // Add data
 addTotalField(data.regional);
@@ -591,14 +617,14 @@ chart.data = data.regional;
 //   "State": "Madhya Pradesh ",
 //   "Active": 104,
 //   "Deaths": 0,
-//   "Cured": 6 
+//   "Cured": 6
 // },
 // {
 //   "State": "Delhi ",
 //  "Active": 444,
 //   "Deaths": 15,
 //   "Cured": 6
-// }, 
+// },
 // {
 //   "State": "Maharashtra ",
 //   "Active": 487,
@@ -636,7 +662,7 @@ function createSeries(field, name) {
   series.dataFields.categoryY = "loc";
   series.stacked = true;
   series.name = name;
-  
+
   var labelBullet = series.bullets.push(new am4charts.LabelBullet());
   labelBullet.locationX = 0.5;
   labelBullet.label.text = "{valueX}";
@@ -710,3 +736,100 @@ $(document).ready(function() {
 
 
 
+//Coro cases choropleth map
+am4core.ready(function() {
+// Themes begin
+am4core.useTheme(am4themes_animated);
+// Themes end
+window.onload = function() {
+
+/*
+jQuery.getJSON( "https://www.amcharts.com/lib/4/geodata/json/india2019Low.json", function( geo ) {
+  if ( am4geodata_data_countries2[ geo.country_code ] !== undefined ) {
+    currentMap = am4geodata_data_countries2[ geo.country_code ][ "maps" ][ 0 ];
+    // add country title
+    if ( am4geodata_data_countries2[ geo.country_code ][ "country" ] ) {
+      title = am4geodata_data_countries2[ geo.country_code ][ "country" ];
+    }
+  }
+});
+*/
+
+  // Default map
+  var defaultMap = "indiaLow";
+  // calculate which map to be used
+  var currentMap = defaultMap;
+  var title = "";
+
+  // Create map instance
+  var chart = am4core.create("amMapdiv", am4maps.MapChart);
+  chart.titles.create().text = title;
+
+  // Set map definition
+  chart.geodataSource.url = "media/amCharts/geodata/json/" + currentMap + ".json";
+  chart.geodataSource.events.on("parseended", function(ev) {
+    /* for(var i = 0; i < ev.target.data.features.length; i++) { polygonSeries.data.push(); } */
+    console.log(ev.target.data.features);
+    $.when( gentAmChartCovCasSeriesDiffObj ).then(function( status ) {
+      console.log(status);
+      polygonSeries.data = amChartSateCovCasSeries;
+    });
+  });
+
+  // Set projection
+  chart.projection = new am4maps.projections.Mercator();
+
+  chart.seriesContainer.draggable = false;
+  chart.seriesContainer.resizable = false;
+  chart.maxZoomLevel = 1;
+
+  // Create map polygon series
+  var polygonSeries = chart.series.push(new am4maps.MapPolygonSeries());
+
+  //Set min/max fill color for each area
+  polygonSeries.heatRules.push({
+    property: "fill",
+    target: polygonSeries.mapPolygons.template,
+    min: chart.colors.getIndex(1).brighten(1),
+    max: chart.colors.getIndex(1).brighten(-0.3)
+  });
+
+  // Make map load polygon data (state shapes and names) from GeoJSON
+  polygonSeries.useGeodata = true;
+
+  // Set up heat legend
+  let heatLegend = chart.createChild(am4maps.HeatLegend);
+  heatLegend.series = polygonSeries;
+  heatLegend.align = "right";
+  heatLegend.width = am4core.percent(25);
+  heatLegend.marginRight = am4core.percent(4);
+  heatLegend.minValue = 0;
+  heatLegend.maxValue = 40000000;
+  heatLegend.valign = "bottom";
+
+  // Set up custom heat map legend labels using axis ranges
+  var minRange = heatLegend.valueAxis.axisRanges.create();
+  minRange.value = heatLegend.minValue;
+  minRange.label.text = "Little";
+  var maxRange = heatLegend.valueAxis.axisRanges.create();
+  maxRange.value = heatLegend.maxValue;
+  maxRange.label.text = "A lot!";
+
+  // Blank out internal heat legend value axis labels
+  heatLegend.valueAxis.renderer.labels.template.adapter.add("text", function(labelText) {
+    return "";
+  });
+
+  // Configure series tooltip
+  var polygonTemplate = polygonSeries.mapPolygons.template;
+  polygonTemplate.tooltipText = "{name}: {value}";
+  polygonTemplate.nonScalingStroke = true;
+  polygonTemplate.strokeWidth = 0.5;
+
+  // Create hover state and set alternative fill color
+  var hs = polygonTemplate.states.create("hover");
+  hs.properties.fill = chart.colors.getIndex(1).brighten(-0.5);
+
+};
+
+}); // end am4core.ready()
